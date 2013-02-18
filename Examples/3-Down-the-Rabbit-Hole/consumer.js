96bcc56b75a6aa3ee11139180451159bfc819548
andrewkeig <andrew.keig@gmail.com>
2013-02-18 06:56:24 +0000

    ex

--- Examples/3-Down-the-Rabbit-Hole/consumer.js
+++ Examples/3-Down-the-Rabbit-Hole/consumer.js
@@ -1,16 +1,13 @@
 //require('../setup').Init('Down the rabbit hole.');
-var connection = require('amqp').createConnection();
+var connect = require('amqp').createConnection();
 
-connection.on('ready', function() {
-    var exchange = connection.exchange('hello.world.exchange', {type: 'direct'});
-    var queue = connection.queue('hello.world.queue');
-
-    queue.on('queueDeclareOk', function(args) {
-        queue.bind(exchange, 'hello.key');
-
-        queue.subscribe(function(message) {
-            console.log(JSON.parse(message.data.toString()
-            ));
+connect.on('ready', function() {
+    var ex = connect.exchange('hello.world.ex', {type: 'direct'});
+    var q = connect.queue('hello.world.q');
+    q.on('queueDeclareOk', function(args) {
+        q.bind(ex, 'hello.key');
+        q.subscribe(function(message) {
+            console.log(JSON.parse(message.data.toString()));
         });
     });
 });
--- Examples/3-Down-the-Rabbit-Hole/producer.js
+++ Examples/3-Down-the-Rabbit-Hole/producer.js
@@ -1,22 +1,21 @@
 //source ~/.bashrc
 //require('../setup').Init('Down the rabbit hole.');
-var connection = require('amqp').createConnection({url: "amqp://guest:guest@localhost:5672"});
-
+var connect = require('amqp').createConnection();
 var messages = [];
 messages.push(' (\\-/)');
 messages.push(" ('.')  –> hello world");
 messages.push('(") (")');
 
-connection.on('ready', function() {
-    var exchange = connection.exchange('hello.world.exchange', {type: 'direct'});
-    var queue = connection.queue('hello.world.queue');
-
-    queue.on('queueDeclareOk', function(args) {
-        queue.bind(exchange, 'hello.key');
-		queue.on('queueBindOk', function() {
-            messages.forEach(function(data) {
-                exchange.publish('hello.key', JSON.stringify(data), {});
-			});
+connect.on('ready', function() {
+    var ex = connect.exchange('hello.world.ex', {type: 'direct'});
+    var q = connect.queue('hello.world.q');
+    q.on('queueDeclareOk', function(args) {
+        q.bind(ex, 'hello.key');
+        q.on('queueBindOk', function() {
+            messages.forEach(function(message) {
+                var data = JSON.stringify(message);
+                ex.publish('hello.key', data, {});
+            });
         });
     });
 });
