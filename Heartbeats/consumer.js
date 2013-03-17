0b831f52bee18ea4afce6c812566893e1fd4b38b
andrewkeig <andrew.keig@gmail.com>
2013-03-17 23:23:06 +0000

    ex

--- Heartbeats/consumer.js
+++ Heartbeats/consumer.js
@@ -1,6 +1,20 @@
-var pubsub = require("./pubsub");
-//var pubsub = new PubSub();
-
-pubsub.Subscribe('shop-queue', function() {
-	console.log('subscribing');
+var connect = require('amqp').createConnection();
+  
+connect.on('ready', function() {
+	Subscribe(connect, 'my.','my.key', function(message) {
+		console.log(message);
+	});
 });
+
+function Subscribe (connect, name, key, callback) {
+    var ex = connect.exchange(name + "exchange");
+    var q = connect.queue(name + "queue");
+        q.on('queueDeclareOk', function(args) {
+        q.bind(name + 'exchange', key);
+        q.on('queueBindOk', function() {
+            q.subscribe(function(message) {
+                callback(message.data.toString());
+            });
+        });
+    });
+};
--- Heartbeats/producer.js
+++ Heartbeats/producer.js
@@ -1,6 +1,22 @@
-var pubSub = require("./pubsub");
-//var pubsub = new PubSub();
+var connect = require('amqp').createConnection();
+var orderId = 0;
 
-setInterval(function(){
-    pubSub.Publish('shop-queue', 'publishing');
-}, 100);
+connect.on('ready', function() {	
+	setInterval(function(){
+	    Publish(connect, 'my.', 'my.key', 'publishing'+ ++orderId);
+	}, 100);
+});
+
+function Publish (connect, name, key, message) {
+    var ex = connect.exchange(name + "exchange");
+    var q = connect.queue(name + "queue");
+    q.on('queueDeclareOk', function(args) {
+      q.bind(name + 'exchange', key);
+      q.on('queueBindOk', function() {
+        ex.publish(key, message);
+        q.close();
+        ex.close();
+      });
+    });
+
+};
